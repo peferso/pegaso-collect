1a85b6d340b7e91edd7c80f7bb4963a0bd68c5a9
peferso <p.fdez.s.90@gmail.com>
2022-01-08 13:43:47 +0100

    developing extractor

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 Created on 3 ene. 2022
 @author: pedfernandez
 '''
+
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By  # for locating elements
+#from selenium.webdriver.chrome.service import Service
+#from selenium.webdriver.common.by import By  # for locating elements
 import time
+import datetime
 import pandas as pd
 from pandas import DataFrame
 
@@ -40,18 +42,28 @@ def run_collection(driver):
             else:
                 continue
 '''
-'''
-driver = webdriver.Chrome('/home/pietari/chromedriver/chromedriver')
-#driver = webdriver.Chrome()
-#clear_cache(driver)
-driver.implicitly_wait(10)
-ipage = 1
-link_1 = "https://www.milanuncios.com/coches-de-segunda-mano/?demanda=n&orden=date&fromSearch=1"
-linkofpage = link_1
+
+link = "https://www.milanuncios.com/coches-de-segunda-mano/?demanda=n&orden=date&fromSearch=1"
+execution_timestamp = datetime.datetime.now()
+
+print('Execution starts:', execution_timestamp)
+
+def accept_terms(browser):
+    button_class = 'sui-AtomButton sui-AtomButton--primary sui-AtomButton--solid sui-AtomButton--center '
+    browser.find_element_by_class_name(button_class).click()
+
+chrome_browser = webdriver.Chrome('/home/pietari/chromedriver/chromedriver')
+chrome_browser.implicitly_wait(10)
+
+chrome_browser.get(link)
 time.sleep(2.5)
-driver.get(linkofpage)
+
+accept_terms(chrome_browser)
 time.sleep(2.5)
-path_element = driver.find_elements(By.CLASS_NAME, "ma-AdList")
+
+print('Execution finished', datetime.datetime.now())
+
+'''path_element = chrome_browser.find_elements(By.CLASS_NAME, "ma-AdList")
 print(len(path_element))
 print(path_element.__class__)
 print(path_element[0])
@@ -71,11 +83,35 @@ print(parsed_html.body.find('div', attrs={'class':'ma-AdList'}).text)
 
 print('FIN')
 '''
-from BeautifulSoup import BeautifulSoup
+'''
+import time
+import pandas as pd
+from pandas import DataFrame
 import urllib.request, urllib.error, urllib.parse
+from bs4 import BeautifulSoup
+import os
 import datetime
+import requests
 
 ts = datetime.datetime.now()
+
+url = 'https://www.milanuncios.com/coches-de-segunda-mano/?demanda=n&orden=date&fromSearch=1'
+
+# This will yield only the HTML code
+response = requests.get(url)
+
+print(response.text)
+
+print(dir(response))
+
+print(response.headers)
+
+print(response.status_code)
+
+exit()
+
+'''
+
 '''
 url = 'https://www.milanuncios.com/coches-de-segunda-mano/?demanda=n&orden=date&fromSearch=1'
 
@@ -87,12 +123,23 @@ webContent = response.read().decode('UTF-8')
 # 1. Navigate to the project local folder with a terminal
 # 2. Make sure that the repository exists 
 # https://stackoverflow.com/questions/25947059/git-clone-repository-not-found/57117454#57117454 comentar que necesité poner password a la key ssh para clonar repositorio privado
+# 3. Get wd in python
 
-f = open('raw-data/ma-data-' + ts.isoformat() + '.html', 'w')
-f.write(webContent)
-f.close()
 '''
-f = open('ma-data-2022-01-04T00:34:57.965655.html', 'r')
+'''
+f = open('pegaso-collect/raw-data/ma-data-' + ts.isoformat() + '.html', 'w')
+f.write(response.text)
+f.close()
+project_folder = os.getcwd()
+raw_data_folder = 'pegaso-collect/raw-data'
+file = project_folder + '/' + raw_data_folder + '/' + 'ma-data-2022-01-04T00:34:57.965655.html'
+f = open(file, 'r')
 html = f.read()
 parsed_html = BeautifulSoup(html, features="html.parser")
-print(parsed_html.body.find('div', attrs={'class':'ma-AdList'}).text)
+#print(parsed_html.body.find('div', attrs={'class':'ma-AdCard-detail'}).text)
+#print( parsed_html.find_all("'class':'ma-AdCard-detail'") )
+
+for foo in parsed_html.find_all('div', attrs={'class': 'ma-AdCard'}):
+    print('here' + foo.text)
+
+'''
